78529a18a346f9e05359f8e8910919d229761990
Cesar Carriel Sandroni <jccarriel@minsait.com>
2025-01-02 10:11:49 -0300

    feat:mod first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod math{
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub fn sub(a: i32, b: i32) -> i32 {
+        a - b
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Addition: {}", math::add(10, 20));
+    println!("Subtraction: {}", math::sub(20, 10));
 }
