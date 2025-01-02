d929a27443fde4b72599273a3f9c3b196515b26f
Cesar Carriel Sandroni <jccarriel@minsait.com>
2025-01-02 10:26:26 -0300

    feat:mod folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-mod math;
+use oper::math::add;
+use oper::math::sub;
+use rand::random;
+
+mod oper;
 
 fn main() {
-    println!("Addition: {}", math::add(10, 20));
-    println!("Subtraction: {}", math::sub(20, 10));
+    println!("Addition: {}", add(10, 20));
+    println!("Subtraction: {}", sub(20, 10));
+    println!("Random number between 1 and 255: {}", random::<u8>());
 }
